3dff9795d12d38e5f1d639cd16e3dc1285042fb6
abdoo303 <abdelrahimabdelazim303@gmail.com>
2024-09-07 16:01:19 +0300

    debug 3

--- Frontend/src/components/PdfView.jsx
+++ Frontend/src/components/PdfView.jsx
@@ -1,7 +1,11 @@
 import { Document, Page } from "react-pdf";
+import { pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+
 import "./PdfView.css";
 import { API_URL } from "../api/auth";
 import { useState } from "react";
+
 function PdfView({ pdf_name }) {
   const url = `${API_URL}/pdfs/${pdf_name}`;
   console.log("url:", url);
@@ -9,10 +13,7 @@ function PdfView({ pdf_name }) {
 
   return (
     <div className="pdf-viewer">
-      <Document
-        file={`${API_URL}/pdfs/${pdf_name}`}
-        onLoadError={(error) => setError(error)}
-      >
+      <Document file={url} onLoadError={(error) => setError(error)}>
         <Page pageNumber={1} />
       </Document>
       {error && <div>Error loading PDF: {error.message}</div>}
