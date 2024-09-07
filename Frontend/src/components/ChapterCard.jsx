25c8fe4af82aad3e55046b48951d1949410997fb
abdoo303 <abdelrahimabdelazim303@gmail.com>
2024-09-07 05:53:59 +0300

    fixed chapterCard

--- Frontend/src/components/ChapterCard.jsx
+++ Frontend/src/components/ChapterCard.jsx
@@ -2,36 +2,43 @@ import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { useNavigate } from "react-router-dom";
+
 import { API_URL } from "../api/auth";
-import "./ChapterCard.css";
 
 function ChapterCard({ chapter }) {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  console.log("chapter is", chapter);
+  console.log("url is", API_URL);
 
   const handleSeeLessonsClick = () => {
-    setLoading(true);
+    setLoading(true); // Set loading state to true when button is clicked
     setTimeout(() => {
+      // Simulate an async action
       navigate(`chapters/${chapter.id}`);
-      setLoading(false);
-    }, 100);
+      setLoading(false); // Reset loading state
+    }, 500); // Adjust the timeout as needed
   };
 
   return (
-    <Card className="chapter-card" style={{ margin: "20px", maxWidth: "600px", padding: "10px" }}>
-      {chapter.image && (
-        <Card.Img variant="top" src={`${API_URL}/images/${chapter.image}`} />
-      )}
+    <Card style={{ margin: "20px", maxWidth: "600px", padding: "10px" }}>
+      <Card.Img variant="top" src={`${API_URL}/images/${chapter.image}`} />
       <Card.Body>
-        <Card.Title className="chapter-title">{chapter.name}</Card.Title>
-        <Button
-          variant="success"
-          onClick={handleSeeLessonsClick}
-          disabled={loading}
-          className="see-lessons-button"
+        <Card.Title style={{ marginBottom: "20px" }}>{chapter.name}</Card.Title>
+        <div
+          className="container-fluid"
+          style={{ alignItems: "center", justifyContent: "center" }}
         >
-          {loading ? "Loading..." : "See Lessons"}
-        </Button>
+          <Button
+            variant="success"
+            onClick={handleSeeLessonsClick}
+            disabled={loading} // Disable the button when loading
+            style={{ marginRight: "15px" }}
+          >
+            {loading ? "Loading..." : "See Lessons"}{" "}
+            {/* Show loading indicator */}
+          </Button>
+        </div>
       </Card.Body>
     </Card>
   );
