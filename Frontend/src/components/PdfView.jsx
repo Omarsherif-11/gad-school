f9d1e4def6b4fdb8083a2cb49f49030e0a0aaba4
abdoo303 <abdelrahimabdelazim303@gmail.com>
2024-09-07 15:50:14 +0300

    debugging...

--- Frontend/src/components/PdfView.jsx
+++ Frontend/src/components/PdfView.jsx
@@ -3,13 +3,21 @@ import "./PdfView.css";
 import { API_URL } from "../api/auth";
 
 function PdfView({ pdf_name }) {
-  return (
-    <div className="pdf-viewer">
-      <Document file={`${API_URL}/pdfs/${pdf_name}`}>
-        <Page pageNumber={1} />
-      </Document>
-    </div>
-  );
+    const url = `${API_URL}/pdfs/${pdf_name}`;
+    console.log("url:", url);
+    const [error, setError] = React.useState(null);
+
+    return (
+      <div className="pdf-viewer">
+        <Document
+          file={`${API_URL}/pdfs/${pdf_name}`}
+          onLoadError={(error) => setError(error)}
+        >
+          <Page pageNumber={1} />
+        </Document>
+        {error && <div>Error loading PDF: {error.message}</div>}
+      </div>
+    );
 }
 
 export default PdfView;
