fdb91fc6693e82268cd3e465a1e461410461b3db
abdoo303 <abdelrahimabdelazim303@gmail.com>
2024-09-07 16:44:55 +0300

    debug 8

--- Frontend/src/components/LessonView.jsx
+++ Frontend/src/components/LessonView.jsx
@@ -1,27 +1,29 @@
 import { useEffect, useState, useRef } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { getLesson, API_URL } from "../api/auth";
-
 import React from "react";
 import ReactPlayer from "react-player";
 import "./lessonView.css";
-
 import Cookies from "js-cookie";
 import PdfViewer from "./PdfView";
+import { FaChevronUp, FaChevronDown } from "react-icons/fa"; // Importing icons
+
 function LessonView() {
   const { id } = useParams();
-
   const [lesson, setLesson] = useState(null);
-
+  const [isPdfVisible, setIsPdfVisible] = useState(true); // State to manage PDF visibility
+  const [activePdf, setActivePdf] = useState(true); // Active state for dropdown
   const navigate = useNavigate();
+  const containerRef = useRef(null);
+  const playerRef = useRef(null);
+  const [isFullscreen, setIsFullscreen] = useState(false);
+  const [playing, setPlaying] = useState(false);
 
   useEffect(() => {
     const getTheLesson = async () => {
       try {
         const response = await getLesson(id);
-
         if (!response) throw new Error("No such lesson");
-
         setLesson(response);
       } catch (err) {
         alert(err.message);
@@ -29,17 +31,12 @@ function LessonView() {
     };
 
     getTheLesson();
-  }, []);
+  }, [id]);
 
   async function goToQuiz() {
     navigate(`quiz/${id}`);
   }
 
-  const containerRef = useRef(null); // Reference to the container div
-  const playerRef = useRef(null);
-  const [isFullscreen, setIsFullscreen] = useState(false);
-
-  // Handle fullscreen change
   const handleFullscreenChange = () => {
     if (
       document.fullscreenElement === containerRef.current ||
@@ -81,7 +78,6 @@ function LessonView() {
     };
   }, [lesson]);
 
-  // Enter fullscreen
   const handleEnterFullscreen = () => {
     if (containerRef.current.requestFullscreen) {
       containerRef.current.requestFullscreen();
@@ -95,18 +91,16 @@ function LessonView() {
   };
 
   const keyFrames = `@keyframes moveOverlay {
-   0% {
-    transform: translateX(-90%); /* Start off-screen to the left */
-  
-  50% {
-    transform: translateX(90%); /* Move off-screen to the right */
-  }
-  100% {
-    transform: translateX(-90%); /* Return to starting position */
-  }
-}`;
-
-  const [playing, setPlaying] = useState(false);
+    0% {
+      transform: translateX(-90%);
+    }
+    50% {
+      transform: translateX(90%);
+    }
+    100% {
+      transform: translateX(-90%);
+    }
+  }`;
 
   const handlePlayPause = () => {
     if (playerRef.current) {
@@ -121,111 +115,135 @@ function LessonView() {
     }
   };
 
-  console.log(API_URL);
+  // Function to toggle PDF visibility
+  const togglePdfVisibility = () => {
+    setActivePdf(!activePdf);
+    setIsPdfVisible(!isPdfVisible);
+  };
 
-   return (
-     <>
-       <div
-         className="container-fluid"
-         style={{
-           marginBottom: "15px",
-           alignContent: "center",
-           alignItems: "center",
-           justifyContent: "center",
-           textAlign: "center",
-         }}
-       >
-         {lesson && (
-           <div
-             className="container-fluid"
-             style={{
-               marginBottom: "15px",
-               alignContent: "center",
-               alignItems: "center",
-               justifyContent: "center",
-               display: "flex",
-               flexDirection: "column",
-               overflow: "hidden",
-             }}
-           >
-             <h1 style={{ marginBottom: "15px" }}>{lesson.name}</h1>
-             <div
-               className="pdf-viewer-container"
-               style={{
-                 width: "75%", // Match the width of the video container
-                 marginBottom: "15px",
-               }}
-             >
-               <PdfViewer url={`${API_URL}/pdfs/${lesson.description}`} />
-             </div>
-             {/* <p style={{ marginBottom: "50px" }}>{lesson.description}</p> */}
-             <style>{keyFrames}</style>
-             <div
-               className="container-fluid"
-               ref={containerRef}
-               style={{
-                 marginBottom: "15px",
-                 alignContent: "center",
-                 alignItems: "center",
-                 textAlign: "center",
-                 justifyContent: "center",
-                 display: "flex",
-                 flexDirection: "column",
-                 width: "75%", // Ensure this matches with PDF width
-                 height: "auto",
-                 position: "relative",
-                 padding: 0,
-               }}
-             >
-               <ReactPlayer
-                 ref={playerRef}
-                 url={`${API_URL}/videos/${lesson.video}`}
-                 controls
-                 width="100%"
-                 height="100%"
-                 config={{
-                   file: {
-                     attributes: {
-                       controlsList: "nodownload", // Prevent download
-                       disablePictureInPicture: true, // Disable PiP
-                     },
-                   },
-                 }}
-                 style={{
-                   maxWidth: "100%",
-                   maxHeight: "100%",
-                   position: "center",
-                   padding: 0,
-                 }}
-                 onPlay={handleEnterFullscreen}
-               />
-               {isFullscreen && (
-                 <p
-                   style={{
-                     position: "absolute",
-                     top: "50%",
-                     left: "50%",
-                     width: "100%",
-                     height: "100%",
-                     zIndex: 9999,
-                     fontSize: "30px",
-                     animation: "moveOverlay 10s linear infinite",
-                     padding: 0,
-                   }}
-                 >
-                   {Cookies.get("email")}
-                 </p>
-               )}
-             </div>
-             <br />
-             <button className="btn btn-success btn-lg" onClick={goToQuiz}>
-               الكويز الخاص بالدرس
-             </button>
-           </div>
-         )}
-       </div>
-     </>
-   );
+  return (
+    <>
+      <div
+        className="container-fluid"
+        style={{
+          marginBottom: "15px",
+          alignContent: "center",
+          alignItems: "center",
+          justifyContent: "center",
+          textAlign: "center",
+        }}
+      >
+        {lesson && (
+          <div
+            className="container-fluid"
+            style={{
+              marginBottom: "15px",
+              alignContent: "center",
+              alignItems: "center",
+              justifyContent: "center",
+              display: "flex",
+              flexDirection: "column",
+              overflow: "hidden",
+            }}
+          >
+            <h1 style={{ marginBottom: "15px" }}>{lesson.name}</h1>
+
+            {/* PDF Toggle Header */}
+            <div
+              className="pdf-header"
+              onClick={togglePdfVisibility}
+              style={{
+                cursor: "pointer",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <h3 style={{ margin: "0" }}>Lesson PDF</h3>
+              <span style={{ marginLeft: "10px" }}>
+                {activePdf ? <FaChevronUp /> : <FaChevronDown />}
+              </span>
+            </div>
+
+            {/* Conditionally render PdfViewer based on isPdfVisible state */}
+            {isPdfVisible && (
+              <div
+                className="pdf-viewer-container"
+                style={{
+                  width: "75%",
+                  marginBottom: "15px",
+                }}
+              >
+                <PdfViewer url={`${API_URL}/pdfs/${lesson.description}`} />
+              </div>
+            )}
+
+            <style>{keyFrames}</style>
+            <div
+              className="container-fluid"
+              ref={containerRef}
+              style={{
+                marginBottom: "15px",
+                alignContent: "center",
+                alignItems: "center",
+                textAlign: "center",
+                justifyContent: "center",
+                display: "flex",
+                flexDirection: "column",
+                width: "75%",
+                height: "auto",
+                position: "relative",
+                padding: 0,
+              }}
+            >
+              <ReactPlayer
+                ref={playerRef}
+                url={`${API_URL}/videos/${lesson.video}`}
+                controls
+                width="100%"
+                height="100%"
+                config={{
+                  file: {
+                    attributes: {
+                      controlsList: "nodownload",
+                      disablePictureInPicture: true,
+                    },
+                  },
+                }}
+                style={{
+                  maxWidth: "100%",
+                  maxHeight: "100%",
+                  position: "center",
+                  padding: 0,
+                }}
+                onPlay={handleEnterFullscreen}
+              />
+              {isFullscreen && (
+                <p
+                  style={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    width: "100%",
+                    height: "100%",
+                    zIndex: 9999,
+                    fontSize: "30px",
+                    animation: "moveOverlay 10s linear infinite",
+                    padding: 0,
+                  }}
+                >
+                  {Cookies.get("email")}
+                </p>
+              )}
+            </div>
+            <br />
+            <button className="btn btn-success btn-lg" onClick={goToQuiz}>
+              الكويز الخاص بالدرس
+            </button>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default LessonView;
