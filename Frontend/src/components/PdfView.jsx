e02edf6ed97d8167bbf0a86054cfa94572ac12b7
abdoo303 <abdelrahimabdelazim303@gmail.com>
2024-09-07 17:09:25 +0300

    debug 10

--- Frontend/src/components/PdfView.jsx
+++ Frontend/src/components/PdfView.jsx
@@ -4,7 +4,12 @@ import "./PdfView.css";
 const PdfView = ({ url }) => {
   return (
     <div className="pdf-viewer-container">
-      <iframe src={url} className="pdf-viewer" title="PDF Viewer" />
+      <iframe
+        src={url}
+        className="pdf-viewer"
+        title="PDF Viewer"
+        onContextMenu={(e) => e.preventDefault()} // Prevent right-click menu
+      />
     </div>
   );
 };
